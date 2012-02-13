568c3394cd8b83bcf895eb58ac1c8459db8451ae
Rubix <fabien.brossier@gmail.com>
2012-02-13 16:58:55 +0100

    Resolved conflict.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120206140503) do
+ActiveRecord::Schema.define(:version => 20120213142113) do
 
   create_table "clients", :force => true do |t|
     t.string   "pass"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "login"
+    t.boolean  "admin"
   end
 
   create_table "evaluations", :force => true do |t|
